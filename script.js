7ecea42df80ed593852db4233c2a3084c817d542
jorgeuri06 <37030019+jorgeuri06@users.noreply.github.com>
2024-05-03 18:23:48 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const joinBtn = document.querySelector('.join-btn');
+
+// Add a click event listener to the button
+joinBtn.addEventListener('click', () => {
+  // Navigate to the page.html file
+  window.location.href = 'page.html';
+});
